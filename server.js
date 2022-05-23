93340f50f2e5f304ac1b89ae09dcdbc526a6e0e4
grayTurtle01 <the.correo@protonmail.com>
2022-05-23 18:49:06 -0500

    flip api

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 http = require('http')
+fs = require('fs')
+
+PORT = process.env.PORT || 8000
 
 http.createServer( (req,res) => {
-    res.write("Hello from http Server")
-    res.end()
+
+    if( req.url == '/api'){
+
+        random = Math.random();
+        flag = random >= 0.5 ? 1: 0
+
+
+        obj = {random: random, flag: flag}
+        json = JSON.stringify(obj)
+
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.write(json)
+        res.end()
+    }
+
+
+    fs.readFile('./public/index.html', (err,data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(data)
+        res.end()
+
+    })
     
-}).listen(process.env.PORT || 8000)
+    
+}).listen(PORT, ()=> console.log(`Listen on port: ${PORT}`))
